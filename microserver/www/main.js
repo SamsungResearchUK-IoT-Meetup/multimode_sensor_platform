96203472ce8f23ef6bfd3d45cc95ff4f124a5d89
Nicholas Herriot <Nicholas.Herriot@gmail.com>
2019-09-17 11:36:03 +0100

    adding images and javascript files to test out PWA

---
+++ microserver/www/main.js
@@ -0,0 +1,17 @@
+
+// Progressive Enhancement (SW supported)
+// if ('serviceWorker' in navigator) {
+if (navigator.serviceWorker) {
+
+  // Register the SW
+  console.log('Attempting to register the service worker....')
+  navigator.serviceWorker.register('/sw.js').then(function(registration){
+
+    console.log('SW Registered');
+
+  }).catch(function (error){
+    console.log('Service Worker registratio error');
+    console.log(error);
+  });
+
+}
